37a08f759366fc119af5244f373d259a2b81958f
rudyxwhite <brandonrwing98@gmail.com>
2022-12-28 19:58:42 -0600

    fixed location of declared variables so now the function works correctly.

--- Develop/script.js
+++ Develop/script.js
@@ -11,94 +11,76 @@ const specialKeys = [ '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 let possiblePass = []
 
 //Empty variable that will be determined the generated password.
-
 let actualPass = ""
 
-// create a function with prompts to determine the length of the password which characters will be used in the generated password
 
-function determinePass(){
-let possibleLength = prompt("How long would you like your password? Please choose between 8 and 128 characters in length.")
 
-//We have to make sure the user submits a valid answer for password length, and then validates the answer.
-
-  if (possibleLength < 8 || possibleLength > 128) {
-    prompt("That isn't a valid amount! Please pick between 8 and 128.")
-  }
-
-  else if (possibleLength > 7 || possibleLength < 129) {
-    alert("Your password will contain " + possibleLength + " characters.")
-  }
-
-//Now that we have a valid length of the password, we determine the results of the other criteria and validate them to the user.
-
-let upperChoice = confirm("Would you like the password to contain uppercase characters?")
-
-  if (upperChoice) {
-    alert("Uppercase characters confirmed.")
-  }
-    else {alert("No uppercase characters will be used!")
-  }
-  
-let lowerChoice = confirm("Would you like the password to contain lowercase characters?")
-
-  if (lowerChoice) {
-    alert("Lowercase characters confirmed.")
-  }
-  else {
-    alert("No lowercase characters will be used!")
-  }
-let numberChoice = confirm("Would you like the password to contain numbers?")
-
-  if (numberChoice) {
-    alert("Numbers confirmed.")
-  }
-  else {
-    alert("No numbers will be used!")
-  }
-let specialChoice = confirm("Would you like the password to contain special characters?")
-  if (specialChoice) {
-    alert("Special characters confirmed.")
-  }
-  else {
-    alert("No special characters will be used!")
-  }
-}
-
-//Now that all the criteria has been picked in a valid way, we must fill the array of possiblePass with said criteria.
-//We do this by pushing the values into the arrays based upon whether or not the user confirmed to use them.
-
-function pushPass(){
-  if (upperChoice){
-    for (i of upperKeys)
-    possiblePass.push(i)
-  }
-  if (lowerChoice){
-    for (i of lowerKeys)
-    possiblePass.push(i)
-  }
-  if (numericChoice){
-    for (i of numericKeys)
-    possiblePass.push(i)
-  }
-  if (specialChoice){
-    for (i of specialKeys)
-    possiblePass.push(i)
-  }
-}
-
-//Finally, we create the password using a for loop.
-
-function createFinal(){
-  for (var i = 0; i < possibleLength; i++) {
-    actualPass += possiblePass[Math.floor(Math.random() * possibleLength)];
-}
-return actualPass;
-}
 
 function generatePassword(){
-  determinePass();
-  pushPass();
-  createFinal();
+
+  // prompts to determine the length of the password that will be used in the generated password
+  let possibleLength = prompt("How long would you like your password? Please choose between 8 and 128 characters in length.")
+
+  //We have to make sure the user submits a valid answer for password length, and then validates the answer.
+  
+    if (possibleLength < 8) {
+      prompt("That isn't a valid amount! Please pick between 8 and 128.")
+    }
+
+    else if (possibleLength > 128) {
+      prompt("That isn't a valid amount! Please pick between 8 and 128.")
+    }
+  
+    else if (possibleLength > 7 || possibleLength < 129) {
+      alert("Your password will contain " + possibleLength + " characters.")
+    }
+    //Now that we have a valid length of the password, we determine the results of the other criteria and validate them to the user.
+  //when the "OK" option is selected in the confirm option, it returns a boolean value of true. When "cancel" is selected, it is false.
+  
+  let upperChoice = confirm("Would you like the password to contain uppercase characters?")
+  
+  if (upperChoice===true) {
+    alert("Uppercase characters confirmed.");
+    possiblePass = possiblePass.concat(upperKeys);
+  }
+    else if(upperChoice===false) {alert("No uppercase characters will be used!")
+  }
+  
+  let lowerChoice = confirm("Would you like the password to contain lowercase characters?")
+  
+  if (lowerChoice===true) {
+    alert("Lowercase characters confirmed.");
+    possiblePass = possiblePass.concat(lowerKeys);
+  }
+  else if (lowerChoice===false) {
+    alert("No lowercase characters will be used!")
+  }
+  let numberChoice = confirm("Would you like the password to contain numbers?")
+  
+  if (numberChoice===true) {
+    alert("Numbers confirmed.");
+    possiblePass = possiblePass.concat(numericKeys);
+  }
+  else if (numberChoice===false) {
+    alert("No numbers will be used!")
+  }
+  let specialChoice = confirm("Would you like the password to contain special characters?")
+  if (specialChoice===true) {
+    alert("Special characters confirmed.");
+    possiblePass = possiblePass.concat(specialKeys);
+  }
+  else if (specialChoice===false) {
+    alert("No special characters will be used!")
+  }
+  //For loop used to generate the password using the array of confirmed characters.
+  //mathfloor and mathrandom will be used
+  for (var i = 0; i < possibleLength; i++) {
+    actualPass += possiblePass[Math.floor(Math.random() * possibleLength)];
+  }
+
+  //we have to return!!!! cannot forget or else it will be undefined!!!
+
+  return actualPass;
 
 }
 
@@ -108,10 +90,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  //I do not know how to remove the current generated password so that clicking starts the process over, so i created an alert
+  //that lets the user know they have to refresh first.
+
+  alert("If you would like to generate another password, simply refresh the page anc click Generate Password again.")
 
 }
 
